46a2a2fc613314034cf092645f9c7a7592b79e0a
Pichini123AO <131919376+Pichini123@users.noreply.github.com>
2023-12-18 14:44:00 -0300

    arreglos2 arreglos2

--- src/models/products.models.js
+++ src/models/products.models.js
@@ -1,18 +1,13 @@
 import { Schema, model } from "mongoose";
 
 const ProductSchema = Schema({
-    name: {
-        type: String,
-        required: [true, 'Name is required.'],
-        unique: true},
-        price: { type: Number, required: [true, "Price is required"] },
-        image: { type: String, required: [true, "image is required"] },
-        description: {type: Boolean, required: [false, "Description is required"] },
-        stock: { type: Number, required: [true, "Stock from is required"] }
-    },
-
+    name: {type: String,required: [true, 'Name is required.'],unique: true},
+    price:{ type: Number, required: [true, "Price is required"] },
+    image:{ type: String, required: [true, "image is required"] },
+    description: {type: Boolean, required: [false, "Description is required"] },
+    stock:{ type: Number, required: [true, "Stock from is required"] },
     
-)
+})
 
 const ProductModel = model('Product', ProductSchema);
 
