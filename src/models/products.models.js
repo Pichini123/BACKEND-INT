403988883f101db826efe24ce9f314d3e06eb6d6
Pichini123AO <131919376+Pichini123@users.noreply.github.com>
2023-12-18 14:39:13 -0300

    Add price, image, description, and stock fields to ProductSchema

--- src/models/products.models.js
+++ src/models/products.models.js
@@ -4,12 +4,15 @@ const ProductSchema = Schema({
     name: {
         type: String,
         required: [true, 'Name is required.'],
-        unique: true
+        unique: true},
+        price: { type: Number, required: [true, "Price is required"] },
+        image: { type: String, required: [true, "image is required"] },
+        description: {type: Boolean, required: [false, "Description is required"] },
+        stock: { type: Number, required: [true, "Stock from is required"] }
     },
-    price: Number,
-    description: String,
-    stock: Number,
-})
+
+    
+)
 
 const ProductModel = model('Product', ProductSchema);
 
